6e285ed8f67156e77ccd2b3ce4ec96248203df53
Khiem Tran <tqkhiem2403@gmail.com>
2017-10-10 23:57:43 -0500

    EXJB-0000: Update code. Should write the script simply

--- installation-scripts/tomcatinstaller.sh
+++ installation-scripts/tomcatinstaller.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
 TOMCAT=apache-tomcat-8.0.23
-TOMCAT_WEBAPPS=$TOMCAT/webapps
-TOMCAT_CONFIG=$TOMCAT/conf/server.xml
-TOMCAT_START=$TOMCAT/bin/startup.sh
+TOMCAT_LOCATION=/usr/local/$TOMCAT
 TOMCAT_ARCHIVE=$TOMCAT.tar.gz
+TOMCAT_WEBAPPS=$TOMCAT_LOCATION/webapps
+TOMCAT_START=$TOMCAT_LOCATION/bin/startup.sh
 TOMCAT_URL=http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.23/bin/apache-tomcat-8.0.23.tar.gz
 
-if [ ! -e $TOMCAT ]; then
-    if [ ! -r $TOMCAT_ARCHIVE ]; then
-            wget $TOMCAT_URL
-    fi
+wget $TOMCAT_URL
 
-    if [ ! -r $TOMCAT_ARCHIVE ]; then
-        echo "Tomcat could not be downloaded." 1>&2
-        echo "Verify that eiter curl or wget is installed." 1>&2
-        echo "If they are, check your internet connection and try again." 1>&2
-        echo "You may also download $TOMCAT_ARCHIVE and place it in this folder." 1>&2
-        exit 1
-    fi
-
-  tar -zxf $TOMCAT_ARCHIVE
-    rm $TOMCAT_ARCHIVE
-fi
-
-if [ ! -w $TOMCAT -o ! -w $TOMCAT_WEBAPPS ]; then
-    echo "$TOMCAT and $TOMCAT_WEBAPPS must be writable." 1>&2
-    exit 1
-fi
+tar xzf $TOMCAT_ARCHIVE -C /usr/local
+rm $TOMCAT_ARCHIVE
 
 # place tomcat customizations here
 
