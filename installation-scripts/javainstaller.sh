dc8353064f232e38c8fed20227d57bf880721ca0
Khiem Tran <tqkhiem2403@gmail.com>
2017-10-11 00:06:44 -0500

    EXJB-0000: Multiple VMExtensions per handler not supported for OS type 'Linux'/ . Combine the scripts for installing Java and Tomcat

--- installation-scripts/javainstaller.sh
+++ installation-scripts/javainstaller.sh
@@ -85,4 +85,20 @@ else
    exit 1
 fi
 
+TOMCAT=apache-tomcat-8.0.23
+TOMCAT_LOCATION=/usr/local/$TOMCAT
+TOMCAT_ARCHIVE=$TOMCAT.tar.gz
+TOMCAT_WEBAPPS=$TOMCAT_LOCATION/webapps
+TOMCAT_START=$TOMCAT_LOCATION/bin/startup.sh
+TOMCAT_URL=http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.23/bin/apache-tomcat-8.0.23.tar.gz
+
+wget $TOMCAT_URL
+
+tar xzf $TOMCAT_ARCHIVE -C /usr/local
+rm $TOMCAT_ARCHIVE
+
+# place tomcat customizations here
+
+$TOMCAT_START
+
 exit 0
